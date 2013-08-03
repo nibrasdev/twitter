beb0e17d0b9dbe5fa3f73ed558c1744c8a401d88
endroid <info@endroid.nl>
2013-08-03 11:56:48 -0400

    Remove unused use statement

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -9,7 +9,6 @@
 
 namespace Endroid\Bundle\TwitterBundle\DependencyInjection;
 
-use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
